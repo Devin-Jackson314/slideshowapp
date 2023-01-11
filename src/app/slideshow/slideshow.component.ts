3c19860a250bbf8e5eec8e2191fd2781a5f019a9
Devin-Jackson314 <you@example.com>
2023-01-11 13:52:57 -0500

    changes to folders for assets

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -18,9 +18,18 @@ export class SlideshowComponent implements OnInit {
 
   getSlides() {
     return this.dataservice.getData().subscribe((data) => {
+      
       this.slideData = data;
       console.log(this.slideData, "first data pull")
+      console.log(this.slideData[0].artist, "artist data")
+     
     })
   }
+
+  //imageSize() {
+    // if(item.type == 'B'){
+        //image.style.width = "px"
+   // }
+  //}
 
 }
